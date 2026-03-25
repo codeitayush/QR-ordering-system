59da41a8a5eed7b6d5ede3afc8bddfed89a5d0fe
Ayush Pathania <ayushpathania994@gmail.com>
2026-03-26 04:18:38 +0530

    token issue

--- restaurant-backend/middleware/authMiddleware.js
+++ restaurant-backend/middleware/authMiddleware.js
@@ -1,20 +1,24 @@
 const jwt = require('jsonwebtoken');
-
+console.log("JWT_SECRET:", process.env.JWT_SECRET)
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    
+
     if (!token) {
       return res.status(401).json({
         success: false,
         message: 'Access denied. No token provided.'
       });
     }
-    
-    const decoded = jwt.verify(token, process.env.ayush_super_secret_key_2026);
+
+    // ✅ FIXED HERE
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.admin = decoded;
     next();
   } catch (error) {
+    console.log("AUTH ERROR:", error.message) // helpful debug
+
     res.status(401).json({
       success: false,
       message: 'Invalid token.'
